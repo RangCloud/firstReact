a89bcb7993e531cf6bb880f5e99b66e17ea1467a
RangCloud <mtr008@naver.com>
2023-11-16 10:19:01 +0900

    231116

---
+++ src/chapter_05/CommentList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Comment from './Commentt';
+
+const CommentList = (props) => {
+  return (
+    <div>
+        <Comment />
+    </div>
+  )
+}
+
+export default CommentList
---
+++ src/chapter_05/Commentt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const comment = (props) => {
+  return (
+    <div>
+        <h1>제가 만든 첫 컴포넌트입니다.</h1>
+    </div>
+  )
+}
+
+export default comment
