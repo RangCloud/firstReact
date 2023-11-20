823ebf6e207bdf28de3c65eb7149613b1f0e4c5c
RangCloud <mtr008@naver.com>
2023-11-20 15:20:46 +0900

    231120

--- src/chapter_12/Calculator.jsx
+++ src/chapter_12/Calculator.jsx
@@ -16,8 +16,50 @@ const toFahrenheit = (celsius) => {
     return (celsius * 9) / 5 + 32;
 }
 
-const Calculator = () => {
-    
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if(Number.isNaN(input)){
+        return "";
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+const Calculator = (props) => {
+    const [temperature, setTemperature] = useState("");
+    const [scale, setScale] = useState("c");
+
+    const handleCelsiusChange = (temperature) => {
+        setTemperature(temperature);
+        setScale("c");
+    };
+
+    const handleFahrenheitChange = (temperature) => {
+        setTemperature(temperature);
+        setScale("f");
+    };
+
+    const celsius =
+        scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit =
+        scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        return(
+            <div>
+                <TemperatureInput 
+                    scale = "c"
+                    temperature = {celsius}
+                    onTemperatureChange = {handleCelsiusChange}
+                />
+                <TemperatureInput 
+                    scale = "f"
+                    temperature = {fahrenheit}
+                    onTemperatureChange = {handleFahrenheitChange}
+                />
+                <BoilingVerdict celsius = {parseFloat(celsius)} />
+            </div>
+        )
 }
 
 export default Calculator;
