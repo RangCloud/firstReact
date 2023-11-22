1b6be929e6761b39b68e707636515cb114bb0e23
RangCloud <mtr008@naver.com>
2023-11-22 10:35:40 +0900

    231122

---
+++ src/chapter_13/Card.jsx
@@ -0,0 +1,20 @@
+const Card = (props) => {
+    const {title, backgroundColor, children} = props;
+
+    return (
+        <div
+            style={{
+                margin: 8,
+                padding: 8,
+                borderRadius: 0,
+                boxShadow: "0px 0px 4px gray",
+                backgroundColor: backgroundColor || "white",
+            }}
+        >
+            {title && <h1>{title}</h1>}
+            {children}
+        </div>
+    );
+}
+
+export default Card;
